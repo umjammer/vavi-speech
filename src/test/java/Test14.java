51c0c702e0e00fc80889e1023dd0126cc5d9bc4f
umjammer <umjammer@gmail.com>
2020-03-14 10:23:11 +0900

    update proof reader

--- src/test/java/Test14.java
+++ src/test/java/Test14.java
@@ -25,14 +25,20 @@ import vavi.speech.phoneme.SudachiJaPhonemer;
 public class Test14 {
 
     /**
-     * @param args 0: file name
+     * @param args 0: file name, 1: exclude regex
      */
     public static void main(String[] args) throws Exception {
         Test14 app = new Test14();
-        app.exec(args[0]);
+        app.exec(args[0], args.length == 2 ? args[1] : "");
     }
 
-    void exec(String file) throws IOException {
+    final static String c1 = (char) 0x1b + "[" + 37 + "m";
+    final static String c0 = (char) 0x1b + "[" + 0 + "m";
+
+    /**
+     * @param exclusionPattern regex
+     */
+    void exec(String file, String exclusionPattern) throws IOException {
         StringBuilder sb = new StringBuilder();
         try (Scanner scanner = new Scanner(SudachiJaPhonemer.class.getResourceAsStream("/sudachi.json"))) {
             while (scanner.hasNextLine()) {
@@ -44,21 +50,35 @@ public class Test14 {
         Tokenizer tokenizer = dict.create();
 
         try (Scanner scanner = new Scanner(new FileInputStream(file))) {
+            int l = 0;
             while (scanner.hasNextLine()) {
-                String line = scanner.nextLine().replaceAll("(［＃.+?］|《.+?》)", ""); // exclude aozora markup tag and ruby
+                String line = scanner.nextLine().replaceAll("(｜|［＃.+?］|《.+?》)", ""); // exclude aozora markup tag and ruby
                 for (Morpheme m : tokenizer.tokenize(Tokenizer.SplitMode.C, line)) {
-                    if (m.isOOV() && containsKanji(m.surface())) {
-System.err.println(m.surface() + "\t" + line);
+                    if (m.isOOV() && containsJaCharacters(m.surface()) &&
+                        !m.surface().matches(exclusionPattern)
+                       ) {
+                        // TODO google suggest
+                        // TODO mark up location in line
+                        String p1 = c1 + line.substring(Math.max(0, m.begin() - 30), m.begin()) + c0;
+                        String p2 = line.substring(m.begin(), m.end());
+                        String p3 = c1 + line.substring(m.end(), Math.min(line.length(), m.end() + 30)) + c0;
+System.err.printf("%s%03d%s\t%s\t%s%s%s\n", c1, l++, c0, m.surface(), p1, p2, p3);
                     }
                 }
             }
         }
     }
 
-    boolean containsKanji(String str) {
+    boolean containsJaCharacters(String str) {
         for (int i = 0;i < str.length(); i++) {
             int code = str.charAt(i);
-            if (code >= 0x4e00 && code <= 0x9fcf) {
+            if (code >= 0x4e00 && code <= 0x9fcf) { // kanji
+                return true;
+            }
+            if (code >= 0x30A0 && code <= 0x30FF) { // katakana
+                return true;
+            }
+            if (code >= 0x3095 && code <= 0x3096) { // hiragana
                 return true;
             }
         }
